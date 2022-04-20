04c36276ece452c7cebc99d36427689ed0b884eb
coleenhuang <coleenhuang@gmail.com>
2022-04-19 17:24:25 -0700

    implemented event handler to get values of number buttons when clicked

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let equation;
+const calculatorButtons= document.querySelector('.button-container')
 
 
-function equation() {
 
+function getNumber() {
+    calculatorButtons.addEventListener('click', (event) => {
+        console.log('clicked')
+        console.log(event.target.value)
+    })
 }
+
+getNumber()
