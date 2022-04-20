7dc9fe801559f0f55c02cac0ad1269ac6e80f4d3
coleenhuang <coleenhuang@gmail.com>
2022-04-20 11:35:39 -0700

    implemented function to display numbers for bottom row of calculator

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let equation;
+let solution = 0;
 const calculatorButtons= document.querySelector('.button-container')
 const equationDisplay = document.querySelector('.top-row')
 const solutionDisplay = document.querySelector('.bottom-row')
@@ -7,13 +8,22 @@ function getNumber() {
     calculatorButtons.addEventListener('click', (event) => {
         
         console.log(event.target.value)
-        equation = event.target.value.toString();
+        
+        if (equation === undefined) {
+            equation = event.target.value.toString();  
+        }
+        solution == 0 ? solution = event.target.value.toString() : solution +=event.target.value.toString()
         displayEquation()
+        displaySolution()
     })
 }
 
 function displayEquation() {
     equationDisplay.innerHTML = equation
+}
+
+function displaySolution() {
+    solutionDisplay.innerHTML = solution
 }
 
 getNumber()
