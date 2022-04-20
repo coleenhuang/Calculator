14a935f88efa877e9d3730b9a6c167a0a42d3c74
coleenhuang <coleenhuang@gmail.com>
2022-04-20 10:28:01 -0700

    implemented function to display value

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 let equation;
 const calculatorButtons= document.querySelector('.button-container')
-
-
+const equationDisplay = document.querySelector('.top-row')
+const solutionDisplay = document.querySelector('.bottom-row')
 
 function getNumber() {
     calculatorButtons.addEventListener('click', (event) => {
-        console.log('clicked')
+        
         console.log(event.target.value)
+        equation = event.target.value.toString();
+        displayEquation()
     })
+}
+
+function displayEquation() {
+    equationDisplay.innerHTML = equation
 }
 
 getNumber()
