bbfd44c1825e28fd46229b7b081f16cec11954e2
coleenhuang <coleenhuang@gmail.com>
2022-04-21 15:51:22 -0700

    added functions to add, subtract, multiply and divide

--- script.js
+++ script.js
@@ -56,8 +56,24 @@ function appendNumber(number, newDigit) {
     return number == 0 || !number ? number = newDigit : number += newDigit
 }
 
-function operate(a, b) {
+function operate(operator, a, b) {
     
+}
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b 
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
 }
 
 
