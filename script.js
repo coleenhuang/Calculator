38afe9f49a0fc44c3eb1d44df79492be42504485
coleenhuang <coleenhuang@gmail.com>
2022-04-21 14:37:05 -0700

    Added data type for buttons

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
-let equation;
-let solution = 0;
+let state = { 
+    firstNum : null,
+    secondNum: null,
+    operator: null,
+    evaluated: false
+}
+let inputDisplay = 0
+
 const calculatorButtons= document.querySelector('.button-container')
 const equationDisplay = document.querySelector('.top-row')
 const solutionDisplay = document.querySelector('.bottom-row')
 
 function getNumber() {
     calculatorButtons.addEventListener('click', (event) => {
-        
-        console.log(event.target.value)
-        
-        if (equation === undefined) {
-            equation = event.target.value.toString();  
+        let buttonType = event.target.dataset.type
+         
+        //sort the buttons by type number, operator or eval
+        /*if (!state.firstNum) {
+            state.firstNum = event.target.value.toString()
+            displayEquation(state.firstNum)
         }
-        solution == 0 ? solution = event.target.value.toString() : solution +=event.target.value.toString()
-        displayEquation()
-        displaySolution()
+        else {
+            if (!state.operator) {
+                state.firstNum == 0 ? state.firstNum = event.target.value.toString() : state.firstNum +=event.target.value.toString()
+                displayEquation(state.firstNum)
+            }
+        }*/
+        
+        
+       
     })
 }
 
-function displayEquation() {
-    equationDisplay.innerHTML = equation
+function displayEquation(solutionNum) {
+    equationDisplay.innerHTML = solutionNum
 }
 
 function displaySolution() {
-    solutionDisplay.innerHTML = solution
+    solutionDisplay.innerHTML = inputDisplay
 }
 
 getNumber()
