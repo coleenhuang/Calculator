45474774701300d4fad5e5958f20869adb5d1c40
coleenhuang <coleenhuang@gmail.com>
2022-04-21 15:36:28 -0700

    implemented switch function for buttons

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-let state = { 
-    firstNum : null,
-    secondNum: null,
-    operator: null,
-    evaluated: false
-}
-let inputDisplay = 0
+let firstNum = 0
+let secondNum= null
+let operator= null
+let evaluated= false
+
 
 const calculatorButtons= document.querySelector('.button-container')
 const equationDisplay = document.querySelector('.top-row')
-const solutionDisplay = document.querySelector('.bottom-row')
+const inputDisplay = document.querySelector('.bottom-row')
 
 function getNumber() {
     calculatorButtons.addEventListener('click', (event) => {
         let buttonType = event.target.dataset.type
-         
+        let valueEntered = event.target.value.toString()
+         switch(buttonType) {
+             /*TODO: case 'evaluate':
+                 if(evaluated || !secondNum || operator ) {
+                     displaySolution()
+                 }
+                 else {
+                     Do the calculation
+                 }
+             */  
+            case 'number':
+            
+                if (!operator){
+                   firstNum = appendNumber(firstNum, valueEntered)
+                   
+                }
+                updateInputScreen()
+                break;
+            case 'operator':
+                if (firstNum) {
+                    operator = valueEntered
+                    updateInputScreen()
+                }
+                break;
+         }
         //sort the buttons by type number, operator or eval
         /*if (!state.firstNum) {
             state.firstNum = event.target.value.toString()
@@ -26,17 +48,31 @@ function getNumber() {
             }
         }*/
         
-        
-       
     })
 }
 
-function displayEquation(solutionNum) {
-    equationDisplay.innerHTML = solutionNum
+function appendNumber(number, newDigit) {
+    console.log(number == 0 )
+    return number == 0 ? number = newDigit : number += newDigit
 }
 
-function displaySolution() {
-    solutionDisplay.innerHTML = inputDisplay
+function evaluate() {
+    
+}
+
+
+
+function updateInputScreen() {
+    if (!secondNum && !operator) {
+        inputDisplay.innerHTML = firstNum
+    }
+    else if (operator) {
+        displayEquation()
+    }
+    
+}
+function displayEquation() {
+    equationDisplay.innerHTML = `${firstNum}${operator}${secondNum?secondNum:''}`
 }
 
 getNumber()
